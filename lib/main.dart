d6eedaf4e60782a6f229c398b5bf1ba0a6bdf52c
Youssef Hamada <yosefhagag84171@gmail.com>
2024-12-23 00:48:51 +0200

    title appbar

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
        home: Scaffold(
+        appBar: AppBar(title: Text('WhatsaApp'),
+        actions: [],
+        ),
         
        ),
     );
