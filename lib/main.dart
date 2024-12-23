7f33ab7449200cd8fb7088a46f1c31cb123a8836
Youssef Hamada <yosefhagag84171@gmail.com>
2024-12-23 17:45:54 +0200

    finish home page ui and BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -5,51 +5,70 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _currentIndex = 0; // مؤشر الصفحة الحالية
+
+  // قائمة الصفحات
+  final List<Widget> _pages = [
+    const HomePage(),
+    const Center(child: Text('Updates')),
+    const Center(child: Text('Communities')),
+    const Center(child: Text('Calls')),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       theme: ThemeData(
         brightness: Brightness.dark,
         //  primarySwatch: Colors.blue,
       ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(onPressed: (){},
+        
+        backgroundColor: Colors.green,child: 
+        const Icon(Icons.add),),
         appBar: AppBar(
           title: const Text('WhatsaApp'),
           actions: [
             IconButton(
                 onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined)),
             IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-            PopupMenuButton(onSelected: (value) {
-              // Handle your action based on the selected value
-            }, itemBuilder: (BuildContext context) {
-              return [
-                PopupMenuItem(
-                  value: () {},
-                  child: const Text('New group'),
-                ),
-                PopupMenuItem(
-                  value: () {},
-                  child: const Text('New broadcast'),
-                ),
-                PopupMenuItem(
-                  value: () {},
-                  child: const Text('Linked devives'),
-                ),
-                PopupMenuItem(
-                  value: () {},
-                  child: const Text('Starred messages'),
-                ),
-                PopupMenuItem(
-                  value: () {},
-                  child: const Text('Settings'),
-                )
-              ];
-            })
+            PopupMenuButton(
+                onSelected: (value) {},
+                itemBuilder: (BuildContext context) {
+                  return [
+                    PopupMenuItem(
+                      value: () {},
+                      child: const Text('New group'),
+                    ),
+                    PopupMenuItem(
+                      value: () {},
+                      child: const Text('New broadcast'),
+                    ),
+                    PopupMenuItem(
+                      value: () {},
+                      child: const Text('Linked devives'),
+                    ),
+                    PopupMenuItem(
+                      value: () {},
+                      child: const Text('Starred messages'),
+                    ),
+                    PopupMenuItem(
+                      value: () {},
+                      child: const Text('Settings'),
+                    )
+                  ];
+                })
           ],
           bottom: PreferredSize(
             preferredSize: const Size.fromHeight(1.0), // Height of the divider
@@ -59,8 +78,37 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        // ignore: prefer_const_constructors
-        body: HomePage(),
+        body: //HomePage(),
+            _pages[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex, // تحديد الصفحة الحالية
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index; // تحديث الصفحة عند التبديل
+            });
+          },
+          selectedItemColor: const Color.fromARGB(255, 78, 185, 51),
+          unselectedItemColor: Colors.white,
+
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.update),
+              label: 'Updates',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.people_outline),
+              label: 'Communities',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.call),
+              label: 'Calls',
+            )
+          ],
+        ),
       ),
     );
   }
