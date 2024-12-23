181d4254f24d75a19f66c9128ba79583bfb3ee0e
Youssef Hamada <yosefhagag84171@gmail.com>
2024-12-23 15:50:45 +0200

    finish appbar ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,50 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-       home: Scaffold(
-        appBar: AppBar(title: Text('WhatsaApp'),
-        actions: [],
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('WhatsaApp'),
+          actions: [
+            IconButton(
+                onPressed: () {}, icon: const Icon(Icons.camera_alt_outlined)),
+            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+            PopupMenuButton(onSelected: (value) {
+              // Handle your action based on the selected value
+            }, itemBuilder: (BuildContext context) {
+              return [
+                PopupMenuItem(
+                  value: () {},
+                  child: const Text('New group'),
+                ),
+                PopupMenuItem(
+                  value: () {},
+                  child: const Text('New broadcast'),
+                ),
+                PopupMenuItem(
+                  value: () {},
+                  child: const Text('Linked devives'),
+                ),
+                PopupMenuItem(
+                  value: () {},
+                  child: const Text('Starred messages'),
+                ),
+                PopupMenuItem(
+                  value: () {},
+                  child: const Text('Settings'),
+                )
+              ];
+            })
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1.0), // Height of the divider
+            child: Container(
+              color: Colors.grey, // Color of the line
+              height: 1.0, // Thickness of the line
+            ),
+          ),
         ),
-        
-       ),
+      ),
     );
   }
 }
